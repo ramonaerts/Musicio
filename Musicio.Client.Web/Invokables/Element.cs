4aff1f8e6f2449c661dc9ebaa64fa0da36fbb726
Aerts,Ramon R.A.R.F <ramon.aerts@student.fontys.nl>
2020-04-30 17:59:35 +0200

    added progress bar skip controls

---
+++ Musicio.Client.Web/Invokables/Element.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace Musicio.Client.Web.Invokables
+{
+    public class Element
+    {
+        protected readonly string id;
+        private readonly IJSRuntime _jsRuntime;
+
+        public Element(string id, IJSRuntime jsRuntime)
+        {
+            this.id = id;
+            _jsRuntime = jsRuntime;
+        }
+
+        public async Task<int> GetOffsetWidth()
+        {
+            return await _jsRuntime.InvokeAsync<int>("get_offsetWidth", id);
+        }
+
+        public async Task<int> GetOffsetLeft()
+        {
+            return await _jsRuntime.InvokeAsync<int>("get_offsetLeft", id);
+        }
+    }
+}
--- Musicio.Client.Web/wwwroot/js/Progress.js
+++ Musicio.Client.Web/wwwroot/js/Progress.js
@@ -1,4 +1,12 @@
-﻿window.functions = () => { };
+﻿window.get_offsetWidth = (id) => {
+    return document.getElementById(id).offsetWidth;
+};
+
+window.get_offsetLeft = (id) => {
+    return document.getElementById(id).offsetLeft;
+};
+
+window.functions = () => { };
 
 window.functions.setTimeout = (time) => {
     setTimeout(function () {
